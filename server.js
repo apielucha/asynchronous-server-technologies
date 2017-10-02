07a728696099d54361d831be1d9b7e9a26ee057a
Alexandre Pielucha <alexandre.pielucha@gmail.com>
2017-10-02 14:50:27 +0200

    Routing implemented

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 module.exports = {
-  logic: function(req, res) {
-    console.log(req.url);
 
-    res.writeHead(200, { 'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+  logic: function(req, res) {
+    var url = req.url;
+
+    switch (url) {
+      case '/admin':
+        res.writeHead(200, { 'Content-Type': 'text/plain'});
+        res.end('This is the admin page.');
+        break;
+      case '/public':
+        res.writeHead(200, { 'Content-Type': 'text/plain'});
+        res.end('This is the public page.');
+        break;
+      default:
+        res.writeHead(400, { 'Content-Type': 'text/plain'});
+        res.end('Error 404, page not found.');
+    }
   },
 
   port: 8080,
   ip: '10.0.2.15'
+
 };
