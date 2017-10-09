444d5e122f1ccba5dd833af28811e4aec99d8858
Alexandre Pielucha <alexandre.pielucha@gmail.com>
2017-10-09 09:38:35 +0200

    Added tests + scripts + travis-ci

---
+++ tests/user.js
@@ -0,0 +1,14 @@
+var should = require('should');
+var user = require('../src/user.js');
+
+describe('User', function() {
+
+  it('save', function(done) {
+    done();
+  });
+
+  it('get', function(done) {
+    done();
+  });
+
+});
